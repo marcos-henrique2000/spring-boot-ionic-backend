f646ede99291309b3ccec679f77ed6ec8f58eb41
Marcos Marins <marcos-henrique2000@hotmail.com>
2020-02-21 16:16:48 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/marcosmarins/services/CategoriaService.java
+++ src/main/java/com/marcosmarins/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.marcosmarins.domain.Categoria;
 import com.marcosmarins.repositories.CategoriaRepository;
+import com.marcosmarins.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + "Tipo: " + Categoria.class.getName()));
 	}
 
 }
